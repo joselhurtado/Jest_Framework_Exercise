fb4b7a620cf4fd42fc0d2de38c6645484a83108f
Jose Hurtado <hello@hurtadojose.com>
2022-07-01 00:45:39 +0000

    Test Suites 1 - Passed 4

--- app.js
+++ app.js
@@ -20,10 +20,6 @@ const sum = (a, b) => {
     return a + b
 }
 
-// just a console log for ourselves.
-console.log(sum(7, 3));
-
-
 // export the function to be used on other files
 // (similar to the keyword `export` when using webpack)
 module.exports = { sum, fromEuroToDollar, fromYenToPound, fromDollarToYen };
